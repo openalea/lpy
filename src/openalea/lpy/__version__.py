854656004302a959d56beeb1129ba96190411e0d
Frederic Boudon <frederic.boudon@cirad.fr>
2008-10-08 07:52:37 +0000

    v1.0.0 release
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/lpy@5581 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/openalea/lpy/__version__.py
+++ src/openalea/lpy/__version__.py
@@ -6,7 +6,7 @@ LPY_VERSION_MAJOR = ((__version_number__ & 0xff0000) >> 16)
 LPY_VERSION_MINOR = ((__version_number__ & 0x00ff00) >> 8)
 LPY_VERSION_REV   = (__version_number__ & 0x0000ff)
 
-LPY_NUM_VERSION_STR = str(LPY_VERSION_MAJOR)+'.'+str(LPY_VERSION_MINOR)+'.'+str(LPY_VERSION_REV)+'rc3'
+LPY_NUM_VERSION_STR = str(LPY_VERSION_MAJOR)+'.'+str(LPY_VERSION_MINOR)+'.'+str(LPY_VERSION_REV)
 
 if len(__revision__) > 0:
     LPY_VERSION_STR = LPY_NUM_VERSION_STR+'-r'+__revision__
