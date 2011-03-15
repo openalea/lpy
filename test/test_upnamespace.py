e043518c9035d121f5705d711aa28ee0c7edf7a8
Frederic Boudon <frederic.boudon@cirad.fr>
2011-03-15 17:15:49 +0000

    - add test with namespace
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/lpy@10276 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ test/test_upnamespace.py
@@ -0,0 +1,41 @@
+from openalea.lpy import *
+
+code = """
+def Start():
+  global VAR
+  assert VAR
+
+Axiom: A(VAR)
+"""
+
+
+def test_namespace_at_creation():
+   l = Lsystem()
+   l.set(code,{'VAR':True})
+   l.iterate()
+
+code = """
+VAR = False
+
+def Start():
+  global VAR
+  assert VAR
+
+Axiom: A(VAR)
+"""
+
+def test_namespace_at_execution():
+   l = Lsystem()
+   l.set(code)
+   l.context().updateNamespace({'VAR':True})
+   l.iterate()
+
+def test_to_failed_namespace_at_execution():
+    l = Lsystem()
+    l.set(code,{'VAR':True})
+    try:
+       l.iterate()
+       assert False and 'Has not failed'
+    except:
+       pass
+   
