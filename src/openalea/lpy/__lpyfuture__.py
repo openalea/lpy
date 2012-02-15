c1e762cfd8b64ebb3f7dea90a586aca83bc1934f
Frederic Boudon <frederic.boudon@cirad.fr>
2012-02-15 20:54:05 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/lpy@11725 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/openalea/lpy/__lpyfuture__.py
+++ src/openalea/lpy/__lpyfuture__.py
@@ -15,17 +15,34 @@ def __check_discard_string(lstring):
    __last_string = lpy.Lstring(lstring)
 
 def enable_string_discard(endeach):
-    if endeach.func_code.co_argcount == 0:
-       if len(args) < 1 : raise ValueError('EndEach should be created with lstring as argument')
-    def wrapped(lstring, *args):
-       global __string_discarded, __last_string
-       if __string_discarded:
-         __string_discarded = False
-         return __last_string
-       res = endeach(lstring, *args)
-       if __string_discarded:
-         __string_discarded = False
-         return __last_string
-       __last_string = lpy.Lstring(lstring)
-       return res
-    return wrapped
+    endeach_arg_nb = endeach.func_code.co_argcount
+    if endeach_arg_nb == 0:
+        def wrapped(lstring):
+           global __string_discarded, __last_string
+           if __string_discarded:
+             __string_discarded = False
+             return __last_string
+           res = endeach()
+           if __string_discarded:
+             __string_discarded = False
+             return __last_string
+           __last_string = lpy.Lstring(lstring)
+           return res
+        return wrapped
+    else:
+        def wrapped(lstring, *args):
+           global __string_discarded, __last_string
+           if __string_discarded:
+             __string_discarded = False
+             return __last_string
+           res = endeach(lstring, *args)
+           if __string_discarded:
+             __string_discarded = False
+             return __last_string
+           __last_string = lpy.Lstring(lstring)
+           return res
+        
+        if endeach_arg_nb == 1:   return wrapped
+        elif endeach_arg_nb == 2: return lambda lstring, lscene : wrapped(lstring, lscene)
+        else:
+            raise ValueError('EndEach should ne defined with maximum 2 arguments.')
