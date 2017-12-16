f840bbde62e60a8c745ba82b5770d30565229b1a
Christophe Pradal <christophe.pradal@inria.fr>
2017-12-16 19:27:10 +0100

    Fix error when setting parameters to sconsx
    the command need to be define before the args:
    python setup.py scons ... install ...

--- conda/build.sh
+++ conda/build.sh
@@ -4,4 +4,4 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
 cp options_conda_build.py options.py
 
-$PYTHON setup.py install --prefix=$PREFIX --scons-ext-param="--implicit-deps-changed"
+$PYTHON setup.py scons --scons-ext-param="--implicit-deps-changed" install --prefix=$PREFIX 
