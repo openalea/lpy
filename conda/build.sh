2a5bce3d146859b9420548991a72013e260f18fa
Christophe Pradal <christophe.pradal@inria.fr>
2017-12-16 19:41:58 +0100

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -4,4 +4,4 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
 cp options_conda_build.py options.py
 
-$PYTHON setup.py scons --scons-ext-param="--implicit-deps-changed" install --prefix=$PREFIX 
+$PYTHON setup.py scons --scons-ext-param=" --implicit-deps-changed " install --prefix=$PREFIX 
