b14ff21b936fd0170d14d1d0c8198ba129ebf378
Frederic Boudon <frederic.boudon@cirad.fr>
2017-12-17 18:07:17 +0100

    add lib and include for plantgl

--- options_conda_build.py
+++ options_conda_build.py
@@ -1,7 +1,10 @@
 import os
 
-PREFIX = os.environ.get('PREFIX')
+PREFIX = os.path.abspath(os.environ.get('PREFIX'))
 SRC_DIR = os.environ.get('SRC_DIR')
+
+vplants_plantgl_include = pj(PREFIX,'include')
+vplants_plantgl_lib = pj(PREFIX,'lib')
 
 if 'CPU_COUNT' in os.environ:
     num_jobs = os.environ['CPU_COUNT']
