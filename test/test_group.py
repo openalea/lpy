70be0ed7d8d605fbbf595a602892db78e452fb5d
Frederic Boudon <frederic.boudon@cirad.fr>
2017-12-22 11:08:41 +0100

    test if pb of tests on travis comes from location of cwd

--- test/test_group.py
+++ test/test_group.py
@@ -1,8 +1,13 @@
 from openalea.lpy import *
 
+
+def get_filename(fname, pymodname = __file__):
+    import os
+    return os.path.join(os.path.dirname(pymodname), fname)
+
 def test_group():
     """ Test group of rules """
-    l = Lsystem('test_group.lpy')
+    l = Lsystem(get_filename('test_group.lpy'))
     l.iterate()
 
 if __name__ == '__main__':
