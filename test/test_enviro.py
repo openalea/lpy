7295d75da3abf1c38bc19a205945e10eac212dfe
Frederic Boudon <frederic.boudon@cirad.fr>
2017-12-22 15:33:27 +0100

    improve test

--- test/test_enviro.py
+++ test/test_enviro.py
@@ -1,8 +1,9 @@
 from openalea.plantgl.all import *
 from openalea.lpy import *
+from testresources import get_filename
 
 def test_enviro():
     """ Test whether environment module ?P works """
-    l = Lsystem('p_h_enviro.lpy')
+    l = Lsystem(get_filename('p_h_enviro.lpy'))
     res = l.iterate()
     assert res[1][0] == Vector3(0,0,10) and "homomorphism not applied before ?P"
