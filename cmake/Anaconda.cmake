ad30ef90557339259f4275e7e7146e7a98ce72a2
Ethan Margaillan <ethan.margaillan@gmail.com>
2019-05-02 15:28:56 +0200

    CMake: Update Conda finder

--- cmake/Anaconda.cmake
+++ cmake/Anaconda.cmake
@@ -3,9 +3,6 @@ if (DEFINED ENV{CONDA_PREFIX})
     # Anaconda Environment
     message(STATUS "Anaconda environment detected.")
     
-    if (WIN32)
-        set(CONDA_ENV "$ENV{CONDA_PREFIX}/Library")
-    else()
-        set(CONDA_ENV "$ENV{CONDA_PREFIX}")
-    endif()
+    set(CONDA_ENV "$ENV{CONDA_PREFIX}/Library")
+    set(CONDA_PYTHON_ENV "$ENV{CONDA_PREFIX}")
 endif()
