feb6c56a7c28c40ad5f95c318e2fe48e9735f723
Ethan Margaillan <ethan.margaillan@gmail.com>
2019-05-27 10:13:23 +0200

    Fix CMake build

--- conda/build.sh
+++ conda/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Working Dir
-mkdir build-cmake
-cd build-cmake
+mkdir build
+cd build
 
 # Build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_BUILD_TYPE=Release ..
 make -j${CPU_COUNT}
 make install
+
+# Install Python Files
+cd ..
+$PYTHON setup.py install --prefix=${PREFIX}
