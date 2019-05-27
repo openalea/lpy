d8c46ae0974fd2c1706d937e1c9ad91c70d8093e
Ethan Margaillan <ethan.margaillan@gmail.com>
2019-05-27 10:13:23 +0200

    Fix CMake build

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,6 +1,6 @@
 :: Working Dir
-mkdir build-cmake
-cd build-cmake
+mkdir build
+cd build
 
 :: Build
 cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release ..
@@ -8,4 +8,9 @@ if errorlevel 1 exit 1
 nmake
 if errorlevel 1 exit 1
 nmake install
+if errorlevel 1 exit 1
+
+:: Install Python Files
+cd ..
+%PYTHON% setup.py install
 if errorlevel 1 exit 1
