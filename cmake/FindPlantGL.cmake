73c2036af1aa90ff2ed75bbd7b718e54f52096a9
Frederic Boudon <frederic.boudon@cirad.fr>
2019-10-18 13:25:08 +0200

    fig bug with cmake file

--- cmake/FindPlantGL.cmake
+++ cmake/FindPlantGL.cmake
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 foreach(v PLANTGL_INCLUDEDIR PLANTGL_LIBRARYDIR)
   set(_env $ENV{${v}})
 endforeach()
@@ -47,31 +46,10 @@ find_library(PLANTGL_SG_LIBRARY
 get_filename_component(PLANTGL_LIBRARY_DIR ${PLANTGL_SG_LIBRARY} DIRECTORY)
 
 if (NOT PLANTGL_LIBRARY_DIR)
-=======
-# Include Directory
-find_path(PLANTGL_INCLUDE_DIR "plantgl/plantgl.h" "libplantgl/plantgl.h" PATHS $ENV{PATH})
-
-# Library Directory
-find_library(PLANTGL_ALGO_LIBRARY NAMES "pglalgo" "libpglalgo" PATHS $ENV{PATH})
-find_library(PLANTGL_GUI_LIBRARY NAMES "pglgui" "libpglgui" PATHS $ENV{PATH})
-find_library(PLANTGL_MATH_LIBRARY NAMES "pglmath" "libpglmath" PATHS $ENV{PATH})
-find_library(PLANTGL_SG_LIBRARY NAMES "pglsg" "libpglsg" PATHS $ENV{PATH})
-find_library(PLANTGL_TOOL_LIBRARY NAMES "pgltool" "libpgltool" PATHS $ENV{PATH})
-
-if (PLANTGL_INCLUDE_DIR AND PLANTGL_ALGO_LIBRARY AND PLANTGL_GUI_LIBRARY AND PLANTGL_MATH_LIBRARY AND PLANTGL_SG_LIBRARY AND PLANTGL_TOOL_LIBRARY)
-    set(PLANTGL_FOUND ON)
-    set(PLANTGL_INCLUDE_DIRS ${PLANTGL_INCLUDE_DIR})
-    set(PLANTGL_LIBRARIES ${PLANTGL_ALGO_LIBRARY} ${PLANTGL_GUI_LIBRARY} ${PLANTGL_MATH_LIBRARY} ${PLANTGL_SG_LIBRARY} ${PLANTGL_TOOL_LIBRARY})
-    
-    # PlantGL found
-    message(STATUS "Found PlantGL: TRUE")
-else()
->>>>>>> 54134afec6f633aeb6e50e2025be517d9adc4368
     set(PLANTGL_FOUND OFF)
     
     if (PlantGL_FIND_REQUIRED)
         # PlantGL not found
-<<<<<<< HEAD
         message(SEND_ERROR "Unable to find PlantGL libraries repository.")
     endif()
 endif()
@@ -119,20 +97,10 @@ if (PLANTGL_INCLUDE_DIR AND PLANTGL_LIBRARY_DIR)
     link_directories(${PLANTGL_LIBRARY_DIRS})
 
 
-=======
-        message(SEND_ERROR "Unable to find PlantGL library.")
-    endif()
-endif()
-
-if (PLANTGL_FOUND)
-    # Build with PlantGL
-    include_directories(${PLANTGL_INCLUDE_DIRS})
->>>>>>> 54134afec6f633aeb6e50e2025be517d9adc4368
     
 elseif (NOT PlantGL_FIND_REQUIRED)
     message(STATUS "Building without PlantGL - Library not found.")
 endif()
-<<<<<<< HEAD
 
 
 # Set PLANTGL_FOUND based only on header location and version.
@@ -166,5 +134,3 @@ else()
   set(PGL_ERROR_REASON
     "${PGL_ERROR_REASON}Unable to find the PlantGL header files. Please set PGL_INCLUDEDIR to the directory containing PlantGL's headers.")
 endif()
-=======
->>>>>>> 54134afec6f633aeb6e50e2025be517d9adc4368
