21afbdd2fe977e61bc39719c2a58ad691cbec226
Frederic Boudon <fredboudon@users.noreply.github.com>
2020-02-02 18:59:12 +0100

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -57,7 +57,7 @@ echo "PYTHON:" ${PYTHON}
 #export PYTHONPATH=${PREFIX}/lib/python${PY_VER}/site-packages/
 ${PYTHON} setup.py install --prefix=${PREFIX} 
 
-cp -r share `python -c "import os, openalea.lpy as lpy ; print(os.path.dirname(lpy.__file__))"`/..
+cp -r share `${PYTHON} -c "import os, openalea.lpy as lpy ; print(os.path.dirname(lpy.__file__))"`/..
 
 echo
 echo "****** CHECK PYTHON LIB"
