d7573dde1af6fa3efc2fe302fa53da4a9f6ceb99
Jonathan LEVY <23365238+j-levy@users.noreply.github.com>
2021-06-14 12:17:13 +0200

    activate conda env is not activated

--- macos_build.sh
+++ macos_build.sh
@@ -1,6 +1,9 @@
 #!/usr/local/bin/bash
+## Note: this script can also be run in ZSH if, for whatever reason, you want to run it in ZSH (the default shell for macOS 10.15+)
+#!/bin/zsh
+
 echo $1
-if [ -d build ] && [ "$1" == "clean" ]; then
+if [[ -d build ]] && [[ "$1" == "clean" ]]; then
     echo "cleaning build directory"
     rm -rf build
 fi
@@ -9,6 +12,24 @@ if ! [[ -d build ]]; then
     echo "creating build directory"
     mkdir build
 fi
+
+export ENV_NAME=lpydev
+if [[ ${CONDA_PREFIX} != ${ENV_NAME} ]] ; then
+    echo "initialize CONDA"
+    if [[ -d $HOME/miniconda3/ ]] ; then
+        source ~/miniconda3/etc/profile.d/conda.sh
+    fi
+    if [[ -d $HOME/anaconda3/ ]] ; then
+        source ~/anaconda3/etc/profile.d/conda.sh
+    fi
+    if [[ ${CONDA_EXE} == "" ]] ; then
+        echo "error activating conda. Did you install it in the default directory? Exit."
+        exit
+    fi
+    conda activate ${ENV_NAME}
+fi
+echo "Conda prefix: ${CONDA_PREFIX}"
+
 
 export PREFIX=${CONDA_PREFIX}
 export PYTHON=${CONDA_PREFIX}/bin/python
@@ -19,7 +40,7 @@ cmake \
       -DPLANTGL_ROOT=../plantgl/build-cmake/ \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_PREFIX_PATH=${PREFIX} \
-      -DCMAKE_BUILD_TYPE=Release  \
+      -DCMAKE_BUILD_TYPE=Debug  \
       -DPython3_EXECUTABLE=${PYTHON} \
       .. 
 
